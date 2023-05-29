37644691b44fab335f9f767132afeed4cec1b23c
dacromp <davidcrompton00@gmail.de>
2023-05-29 11:45:58 +0200

    init

---
+++ app-files/data.js
@@ -0,0 +1,132 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-whatsapp-image-2023-05-10-at-175653",
+      "name": "WhatsApp Image 2023-05-10 at 17.56.53",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1806374044945063,
+          "pitch": 0.057059288655517904,
+          "rotation": 0,
+          "target": "2-whatsapp-image-2023-05-10-at-175705"
+        },
+        {
+          "yaw": -2.9107050255240505,
+          "pitch": 0.04170883311794071,
+          "rotation": 0,
+          "target": "1-whatsapp-image-2023-05-10-at-175659"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0018888340170555,
+          "pitch": 0.24187007115860482,
+          "title": "Motorrad",
+          "text": "Brumbrum"
+        },
+        {
+          "yaw": -1.9460776868201144,
+          "pitch": 0.2785064287209753,
+          "title": "Papertrash",
+          "text": "Nice"
+        }
+      ]
+    },
+    {
+      "id": "1-whatsapp-image-2023-05-10-at-175659",
+      "name": "WhatsApp Image 2023-05-10 at 17.56.59",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": 0.86453224973428,
+        "pitch": 0.35653946996900565,
+        "fov": 1.3268945324871384
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.31493656129268643,
+          "pitch": 0.1330960830832666,
+          "rotation": 0,
+          "target": "0-whatsapp-image-2023-05-10-at-175653"
+        },
+        {
+          "yaw": 1.2494690380271454,
+          "pitch": 0.13015385882450303,
+          "rotation": 0,
+          "target": "2-whatsapp-image-2023-05-10-at-175705"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-whatsapp-image-2023-05-10-at-175705",
+      "name": "WhatsApp Image 2023-05-10 at 17.57.05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2924871189171565,
+          "pitch": 0.01589088037466979,
+          "rotation": 0,
+          "target": "0-whatsapp-image-2023-05-10-at-175653"
+        },
+        {
+          "yaw": -1.8623858745286697,
+          "pitch": 0.0872486062960327,
+          "rotation": 0,
+          "target": "1-whatsapp-image-2023-05-10-at-175659"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
